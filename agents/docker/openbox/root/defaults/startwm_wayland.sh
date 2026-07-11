ddfc2d41bb091cd82118349b79125b3f62974c7f
Eunjin Park <ejpark78@gmail.com>
2026-07-11 14:18:14 +0900

    feat(openbox): zsh default shell, caps:swapescape, UID/GID build args, split volumes
    - Taskfile.yml: restart task down -> up -d
    - Dockerfile: add PUID/PGID build args, usermod + chown /config
    - compose.yml: caps:swapescape, XDG_RUNTIME_DIR, split sandbox volumes, build args
    - startwm_wayland.sh: caps:swapescape, XDG_RUNTIME_DIR, /tmp/.X11-unix cleanup
    - init-vscode-config/run: zsh default profile, Openbox autostart setxkbmap,
      oh-my-zsh UID fix, /config/.cache

--- agents/docker/openbox/root/defaults/startwm_wayland.sh
+++ agents/docker/openbox/root/defaults/startwm_wayland.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 # Start DE
+rm -rf /tmp/.X11-unix
 ulimit -c 0
 export XCURSOR_THEME=breeze_cursors
 export XCURSOR_SIZE=24
 export XKB_DEFAULT_LAYOUT=us
 export XKB_DEFAULT_RULES=evdev
+export XKB_DEFAULT_OPTIONS=caps:swapescape
 export WAYLAND_DISPLAY=wayland-1
 
 # labwc menu 강제 설정 (볼륨 persist 대응)
 cp /defaults/menu.xml ${HOME}/.config/labwc/menu.xml
 cp /defaults/menu.xml ${HOME}/.config/labwc/menu_wayland.xml
+
+export XDG_RUNTIME_DIR=/config/.runtime
 
 if [ "${SELKIES_DESKTOP}" == "true" ]; then
   labwc > /dev/null 2>&1 &
